c6ef6fb6d57b5adcda820c5df17bff1694902619
Simon Neutert <simon.neutert@gmail.com>
2019-03-14 19:02:42 +0100

    send request

--- simple/Read.py
+++ simple/Read.py
@@ -2,12 +2,25 @@
 
 import RPi.GPIO as GPIO
 import SimpleMFRC522
+import signal
+import urllib2
+import requests
+import time
+
+continue_reading = True
 
 reader = SimpleMFRC522.SimpleMFRC522()
 
-try:
+while continue_reading:
+
+    try:
         id, text = reader.read()
+        url = "http://localhost:8081/user/" + str(id)
+        print(url)
+        r = requests.post(url)
+        print(r.content)
         print(id)
         print(text)
-finally:
+        time.sleep(5)
+    finally:
         GPIO.cleanup()
